0985ddc5bf9f0735251f5081eefe5dbcc1a9284d
Norman Misael Joya Lainez <njoya@statetrust.sv>
2017-04-28 15:33:58 -0600

    Initial Commit
    Initial commit for udemy's course:
    https://www.udemy.com/aspnet-mvc-quick-start

---
+++ ConctWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConctWeb.Startup))]
+namespace ConctWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
